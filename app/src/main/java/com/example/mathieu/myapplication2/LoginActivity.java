baf3ba5592f67da0e693d91c3a8468177fef9f61
Sharaf <sharafgabel@gmail.com>
2015-11-16 19:55:47 +0100

    Création de l'activité login, modification de l'activité principale, création d'une classe personne, création de login.php, permettant de vérifier si le login et mot de passe renseigné sont bien dans la base de données, création d'un fichier main.xml, et modification du androidManifest.xml ( pour ajouter la nouvelle activité )

---
+++ app/src/main/java/com/example/mathieu/myapplication2/LoginActivity.java
@@ -0,0 +1,349 @@
+package com.example.mathieu.myapplication2;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Created by Sharaf on 16/11/2015.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+    // Lien vers votre page php sur votre serveur
+    private static final String	UPDATE_URL	= "http://paddesite.pa.ohost.de/login.php";
+
+    public ProgressDialog progressDialog;
+
+    private EditText UserEditText;
+
+    private EditText PassEditText;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // initialisation d'une progress bar
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(false);
+        // Récupération des éléments de la vue définis dans le xml
+        UserEditText = (EditText) findViewById(R.id.username);
+
+        PassEditText = (EditText) findViewById(R.id.password);
+        Button button = (Button) findViewById(R.id.okbutton);
+
+        // Définition du listener du bouton
+        button.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+
+                int usersize = UserEditText.getText().length();
+
+                int passsize = PassEditText.getText().length();
+                // si les deux champs sont remplis
+                if (usersize > 0 && passsize > 0)
+                {
+
+                    progressDialog.show();
+
+                    String user = UserEditText.getText().toString();
+
+                    String pass = PassEditText.getText().toString();
+                    // On appelle la fonction doLogin qui va communiquer avec le PHP
+                    doLogin(user, pass);
+
+                }
+                else
+                    createDialog("Error", "Please enter Username and Password");
+
+            }
+
+        });
+
+        button = (Button) findViewById(R.id.cancelbutton);
+        // Création du listener du bouton cancel (on sort de l'appli)
+        button.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+                quit(false, null);
+            }
+
+        });
+
+    }
+
+    private void quit(boolean success, Intent i)
+    {
+        // On envoie un résultat qui va permettre de quitter l'appli
+        setResult((success) ? Activity.RESULT_OK : Activity.RESULT_CANCELED, i);
+        finish();
+
+    }
+
+    private void createDialog(String title, String text)
+    {
+        // Création d'une popup affichant un message
+        AlertDialog ad = new AlertDialog.Builder(this)
+                .setPositiveButton("Ok", null).setTitle(title).setMessage(text)
+                .create();
+        ad.show();
+
+    }
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+    private void doLogin(final String login, final String pass)
+    {
+
+        final String pw = md5(pass);
+        // Création d'un thread
+        Thread t = new Thread()
+        {
+
+            public void run()
+            {
+
+                Looper.prepare();
+                try {
+                    HttpURLConnection connection = (HttpURLConnection)new URL(UPDATE_URL).openConnection();
+                    connection.setDoOutput(true);
+                    connection.setChunkedStreamingMode(0);
+                    connection.setRequestMethod("POST");
+
+                    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+
+                    HashMap<String,String> map = new HashMap<String,String>();
+                    map.put("username",login);
+                    map.put("password", pw);
+
+                    writer.write(getPostDataString(map));
+                    writer.flush();
+                    writer.close();
+                    String json;
+                    int responseCode = connection.getResponseCode();
+                    if(responseCode == HttpURLConnection.HTTP_OK)
+                    {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder sb=new StringBuilder();
+                        int cp;
+                        while((cp=in.read())!=-1){
+                            sb.append((char)cp);
+                        }
+                        json= sb.toString();
+                    }
+                    else
+                    {
+                        json="Erreur ";
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Looper.loop();
+
+            }
+
+        };
+
+        t.start();
+
+    }
+
+    private void read(InputStream in)
+    {
+        // On traduit le résultat d'un flux
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+
+        SAXParser sp;
+
+        try
+        {
+
+            sp = spf.newSAXParser();
+
+            XMLReader xr = sp.getXMLReader();
+            // Cette classe est définie plus bas
+            LoginContentHandler uch = new LoginContentHandler();
+
+            xr.setContentHandler(uch);
+
+            xr.parse(new InputSource(in));
+
+        }
+        catch (ParserConfigurationException e)
+        {
+
+        }
+        catch (SAXException e)
+        {
+
+        }
+        catch (IOException e)
+        {
+        }
+
+    }
+
+    private String md5(String in)
+    {
+
+        MessageDigest digest;
+
+        try
+        {
+
+            digest = MessageDigest.getInstance("MD5");
+
+            digest.reset();
+
+            digest.update(in.getBytes());
+
+            byte[] a = digest.digest();
+
+            int len = a.length;
+
+            StringBuilder sb = new StringBuilder(len << 1);
+
+            for (int i = 0; i < len; i++)
+            {
+
+                sb.append(Character.forDigit((a[i] & 0xf0) >> 4, 16));
+
+                sb.append(Character.forDigit(a[i] & 0x0f, 16));
+
+            }
+
+            return sb.toString();
+
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    private class LoginContentHandler extends DefaultHandler
+    {
+        // Classe traitant le message de retour du script PHP
+        private boolean	in_loginTag		= false;
+        private int			userID;
+        private boolean	error_occured	= false;
+
+        public void startElement(String n, String l, String q, Attributes a) throws SAXException
+        {
+
+            if (l == "login")
+                in_loginTag = true;
+            if (l == "error")
+            {
+
+                progressDialog.dismiss();
+
+                switch (Integer.parseInt(a.getValue("value")))
+                {
+                    case 1:
+                        createDialog("Error", "Couldn't connect to Database");
+                        break;
+                    case 2:
+                        createDialog("Error", "Error in Database: Table missing");
+                        break;
+                    case 3:
+                        createDialog("Error", "Invalid username and/or password");
+                        break;
+                }
+                error_occured = true;
+
+            }
+
+            if (l == "user" && in_loginTag && a.getValue("id") != "")
+                // Dans le cas où tout se passe bien on récupère l'ID de l'utilisateur
+                userID = Integer.parseInt(a.getValue("id"));
+
+        }
+
+        public void endElement(String n, String l, String q) throws SAXException
+        {
+            // on renvoie l'id si tout est ok
+            if (l == "login")
+            {
+                in_loginTag = false;
+
+                if (!error_occured)
+                {
+                    progressDialog.dismiss();
+                    Intent i = new Intent();
+                    i.putExtra("userid", userID);
+                    quit(true, i);
+                }
+            }
+        }
+
+        public void characters(char ch[], int start, int length)
+        {
+        }
+
+        public void startDocument() throws SAXException
+        {
+        }
+
+        public void endDocument() throws SAXException
+        {
+        }
+}
+}
--- app/src/main/java/com/example/mathieu/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/mathieu/myapplication2/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.mathieu.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    public static final int RESULT_Main = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startActivityForResult(new Intent(MainActivity.this, LoginActivity.class), RESULT_Main);
+
+        tv = new TextView(this);
+        setContentView(tv);
         setContentView(R.layout.activity_main);
+    }
+
+    private void startup(Intent i)
+    {
+        // Récupère l'identifiant
+        int user = i.getIntExtra("userid",-1);
+
+        //Affiche les identifiants de l'utilisateur
+        tv.setText("UserID: "+String.valueOf(user)+" logged in");
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == RESULT_Main && resultCode == RESULT_CANCELED)
+            finish();
+        else
+            startup(data);
     }
 }
