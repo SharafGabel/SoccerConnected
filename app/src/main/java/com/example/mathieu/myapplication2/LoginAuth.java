f1c1facbf07999a9337cc4374f5326fe10313105
Yacine <chakouryacine@hotmail.fr>
2015-11-17 23:35:14 +0100

    Classe de connection à la BD

---
+++ app/src/main/java/com/example/mathieu/myapplication2/LoginAuth.java
@@ -0,0 +1,40 @@
+package com.example.mathieu.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Yacine on 17/11/2015.
+ */
+public class LoginAuth extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginauth);
+        Button b = (Button) this.findViewById(R.id.connectB);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondeActivite = new Intent(LoginAuth.this, MenuApp.class);
+                startActivity(secondeActivite);
+            }
+        });
+
+    }
+
+    public void onClick(View v) {
+        if (v == this.findViewById(R.id.connectB)) {
+            Intent intent = new Intent(this, MenuApp.class);
+            startActivity(intent);
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/mathieu/myapplication2/MenuApp.java
@@ -0,0 +1,16 @@
+package com.example.mathieu.myapplication2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Yacine on 17/11/2015.
+ */
+public class MenuApp extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
