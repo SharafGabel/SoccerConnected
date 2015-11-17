b5704773a5d9e719d1ecc59d34c7db64ef7f44e9
Yacine <chakouryacine@hotmail.fr>
2015-11-17 15:19:42 +0100

    Classe de connection à la BD

---
+++ app/src/main/java/com/example/mathieu/myapplication2/ConnexionHTTP.java
@@ -0,0 +1,21 @@
+package com.example.mathieu.myapplication2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Yacine on 17/11/2015.
+ */
+public class ConnexionHTTP {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/mathieu/myapplication2/RegisterInActivity.java
@@ -0,0 +1,16 @@
+package com.example.mathieu.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yacine on 17/11/2015.
+ */
+public class RegisterInActivity extends AppCompatActivity {
+
+
+
+
+
+
+
+}
