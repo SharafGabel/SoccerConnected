48f6e9e5d55811aaa81dbf7544f4cb3565fa6f7b
Yacine <chakouryacine@hotmail.fr>
2015-11-18 17:55:58 +0100

    Classe de connection à la BD

--- app/src/main/java/activity/MenuApp.java
+++ app/src/main/java/activity/MenuApp.java
@@ -5,25 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.mathieu.myapplication2.R;
 
 /**
  * Created by Yacine on 17/11/2015.
  */
-public class MenuApp extends Activity {
+public class MenuApp extends Activity  implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = (Button) this.findViewById(R.id.createEvent);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent secondeActivite = new Intent(MenuApp.this, EventActivity.class);
-                startActivity(secondeActivite);
-            }
-        });
+        ImageButton b = (ImageButton) this.findViewById(R.id.menu_event);
+        b.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        if (v == this.findViewById(R.id.createEvent)) {
+            Intent intent = new Intent(MenuApp.this, EventActivity.class);
+            startActivity(intent);
+
+        }
     }
 }
