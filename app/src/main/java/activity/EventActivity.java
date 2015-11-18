5e60106261a8a4fa90120fdca972faa4bb618895
Sharaf <sharafgabel@gmail.com>
2015-11-18 17:38:30 +0100

    Réorganisation diagrammes

---
+++ app/src/main/java/activity/EventActivity.java
@@ -0,0 +1,20 @@
+package activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mathieu.myapplication2.R;
+
+/**
+ * Created by Sharaf on 18/11/2015.
+ */
+public class EventActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_event);
+    }
+
+
+}
--- app/src/main/java/activity/MenuApp.java
+++ app/src/main/java/activity/MenuApp.java
@@ -1,7 +1,10 @@
 package activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mathieu.myapplication2.R;
 
@@ -14,5 +17,13 @@ public class MenuApp extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) this.findViewById(R.id.createEvent);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondeActivite = new Intent(MenuApp.this, EventActivity.class);
+                startActivity(secondeActivite);
+            }
+        });
     }
 }
