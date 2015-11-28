ac4a178ccec077632d52e015c0f87373f90b9319
Sharaf <sharafgabel@gmail.com>
2015-11-28 22:24:59 +0100

    Code de listEventActivity arrangé

--- app/src/main/java/activity/listEventActivity.java
+++ app/src/main/java/activity/listEventActivity.java
@@ -31,7 +31,7 @@ import java.util.Map;
  */
 public class listEventActivity extends AppCompatActivity {
 
-    private static final String	LIST_EVENT_URL	= "https://footapp-sharaf.c9users.io/ConnectedSoccerPhp/web/api/events";
+    private static final String	LIST_EVENT_URL= "https://footapp-sharaf.c9users.io/ConnectedSoccerPhp/web/api/events";
     private String jsonString;
     List<Map<String,String>> events = new ArrayList<Map<String,String>>();
     ListView mListView;
