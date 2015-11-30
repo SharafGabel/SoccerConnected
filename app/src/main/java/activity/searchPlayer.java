32e5a9ab1b7ae2f07603419dba2fb501a98a9c02
Yacine <chakouryacine@hotmail.fr>
2015-11-30 14:21:39 +0100

    Ajout de l'activité permettant de lister les évènements

--- app/src/main/java/activity/searchPlayer.java
+++ app/src/main/java/activity/searchPlayer.java
@@ -1,6 +1,7 @@
 package activity;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,13 +11,22 @@ import android.widget.Toast;
 
 import com.example.mathieu.myapplication2.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Yacine on 30/11/2015.
@@ -26,6 +36,9 @@ public class searchPlayer extends AppCompatActivity {
     private SearchView search;
     private static String URL_RESEARCH_PLAYER;
     private ListView listeResult;
+    private List<String> resultSearch;
+    private List<String> resultID;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,7 +66,7 @@ public class searchPlayer extends AppCompatActivity {
 
                 Toast.makeText(getBaseContext(), query,
                         Toast.LENGTH_SHORT).show();
-                searchPlayerDb(query);
+                serachThread(query);
                 return false;
             }
 
@@ -66,25 +79,64 @@ public class searchPlayer extends AppCompatActivity {
         });
     }
 
+    private void serachThread(final String recherche)
+    {
+
+
+        Thread t = new Thread() {
+            public void run() {
+                Looper.prepare();
+                searchPlayerDb(recherche);
+                Looper.loop();
+            }
+        };
+        t.start();
+
+        if(!t.isInterrupted())
+        {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void searchPlayerDb(String recherche)
     {
         try{
-            URL_RESEARCH_PLAYER="";
+            URL_RESEARCH_PLAYER="https://footapp-sharaf.c9users.io/ConnectedSoccerPhp/web/api/recherches/usernames";
             URL url = new URL(URL_RESEARCH_PLAYER);
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setDoOutput(true);
             connection.setDoInput(true);
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod("POST");
 
+                    /*A ne jamais mettre lorsque l'on récupère des données json*/
+            //connection.setChunkedStreamingMode(0);
+
+
+            HashMap<String,String> map = new HashMap<String,String>();
+            map.put("username",recherche);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(getPostDataString(map));
+            writer.flush();
+            writer.close();
+            String json;
             int responseCode = connection.getResponseCode();
             if(responseCode == HttpURLConnection.HTTP_OK)
             {
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-                while ((line = in.readLine()) != null) {
+                StringBuilder sb=new StringBuilder();
+                String line=null;
+                while( (line=in.readLine())!=null)
+                {
                     sb.append(line);
                 }
-                jsonString = sb.toString();
+
+                json = sb.toString();
+                initDetails(json);
+                //createDialog("Test",json);
             }
             //connection.disconnect();
         } catch (MalformedURLException e) {
@@ -108,6 +160,43 @@ public class searchPlayer extends AppCompatActivity {
                 .create();
         ad.show();
     }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+
+    private void initDetails(String jsonString){
+        try{
+            //
+             JSONObject jsonResponse = new JSONObject(jsonString);
+            JSONObject jsonMainNode = jsonResponse.optJSONObject("users");
+
+            //String username = jsonMainNode.optString("username");
+            //String id = jsonMainNode.optString("id");
+
+            createDialog("test",jsonString);
+            //createDialog("test2",jsonMainNode);
+
+            //this.resultSearch
+        }
+        catch(JSONException e){
+            Toast.makeText(getApplicationContext(), "Error"+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 
 
